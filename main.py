6bfef9950e7d02b5c5a14e1e6acc15bed736b31d
Naaav64 <naaav@protonmail.com>
2025-07-07 07:33:07 -0400

    v1.0 complete!

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import sys
 from stats import get_num_words, get_character_num, get_report
 
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     print(f"============ BOOKBOT ============\nAnalyzing book found at {book_path}\n----------- Word Count ----------")
     print(f"Found {get_num_words(text)} total words\n--------- Character Count -------")
@@ -13,4 +14,9 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
     
-main()
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+elif len(sys.argv) == 2:
+    main()
